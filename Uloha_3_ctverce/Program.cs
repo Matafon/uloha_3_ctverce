714b242aafdd7dafd8670aad37f407337f58cb58
Matafon <79162313+Matafon@users.noreply.github.com>
2024-09-13 20:35:24 +0200

    Add files via upload

---
+++ Uloha_3_ctverce/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Zadej velikost čtverce:");
+int ctverec = Convert.ToInt32(Console.ReadLine());
+
+if (ctverec == 1)
+{
+    Console.WriteLine("*");
+}
+
+else
+{
+    string mezera = new string(' ', ctverec - 2);
+    string hvezda = "*";
+    string strana = new string('*', ctverec);
+
+    Console.WriteLine(strana);
+    for (int j = 0; j < ctverec - 2; j++)
+    {
+        Console.WriteLine(hvezda + mezera + hvezda);
+    }
+    Console.WriteLine(strana);
+}
